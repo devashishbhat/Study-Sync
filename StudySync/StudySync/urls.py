ddb6bf0e0739331c7b9c87e2229b8a273a0aa9b3
devashishbhat <devashishbhat11@gmail.com>
2024-01-23 14:36:02 +0530

    Added image field

--- StudySync/StudySync/urls.py
+++ StudySync/StudySync/urls.py
@@ -2,6 +2,10 @@
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 
 urlpatterns = [
@@ -9,3 +13,5 @@ urlpatterns = [
     path('',include('projects.projectsurls')),
    
 ]
+
+urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
